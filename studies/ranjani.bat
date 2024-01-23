b7715df5b0bf9fd451d809ebb0f821b000790ab1
Jack Kolb <jack@jackkolb.com>
2024-01-23 16:18:32 -0500

    Update ranjani.bat

--- studies/ranjani.bat
+++ studies/ranjani.bat
@@ -1,6 +1,6 @@
 echo "Now starting the user study!"
 # Open Microsoft Edge
-start msedge google.com
+start msedge jackkolb.com
 echo "All set up!"
 # Keep the terminal open until the ENTER key is pressed
 read
